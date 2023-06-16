951ac4eabe1dc27490fd2cb287389ada41697e1a
Transcendence-1 <alexiscolin0805@gmail.com>
2023-06-16 20:12:07 +0900

    model commit

--- src/main/java/com/example/backend/model/User.java
+++ src/main/java/com/example/backend/model/User.java
@@ -1,4 +1,30 @@
 package com.example.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "emial")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 }
